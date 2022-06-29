ca34ae4c77350bc4a2dd39f1b5f67ce2b6bf5c61
clwest <chris@donkeybetz.com>
2022-06-29 12:25:08 -0600

    typo

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,7 +2,7 @@ import {Contract, providers} from "ethers";
 import {formatEther} from "ethers/lib/utils";
 import Head from "next/head";
 import {useEffect, useRef, useState} from "react";
-import Web3Modal from "Web3modal";
+import Web3Modal from "web3modal";
 import {
   CRYPTODEVS_DAO_ABI,
   CRYPTODEVS_DAO_CONTRACT_ADDRESS,
